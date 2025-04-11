e20c521339321d182bc5296c527acd9f4ad83dc9
Haba <karabasov.haba@mail.ru>
2025-04-11 15:14:17 +0300

    service provider fix

--- src/Http/Controllers/V1/Shop/Customer/PhoneAuthController.php
+++ src/Http/Controllers/V1/Shop/Customer/PhoneAuthController.php
@@ -177,6 +177,7 @@ class PhoneAuthController extends CustomerController
         $validator = Validator::make($request->all(), [
             'phone' => 'required',
             'code'  => 'required',
+            'device_name' => 'nullable',
         ]);
 
         if ($validator->fails()) {
@@ -218,7 +219,7 @@ class PhoneAuthController extends CustomerController
         $customer->tokens()->delete();
 
         // Создаем новый токен
-        $token = $customer->createToken(['role:customer'])->plainTextToken;
+        $token = $customer->createToken($request->device_name, ['role:customer'])->plainTextToken;
 
         // Очищаем данные из кэша
         \Cache::forget('phone_verification_code_' . $request->phone);
