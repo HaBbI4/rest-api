d6f8e8c1b9b9639f66207955ec79fb85b670a252
Haba <karabasov.haba@mail.ru>
2025-04-21 16:03:58 +0300

    Правки по товару в АПИ

--- src/Http/Resources/V1/Shop/Catalog/ProductResource.php
+++ src/Http/Resources/V1/Shop/Catalog/ProductResource.php
@@ -27,60 +27,34 @@ class ProductResource extends JsonResource
 
         /* Проверяем, является ли пользователь оптовым клиентом */
         $isWholesaleCustomer = false;
-        $debugInfo = [];
         
         // Получаем токен из заголовка Authorization
         $token = $request->bearerToken();
-        $debugInfo['has_token'] = !empty($token);
         
         // Если есть токен, пытаемся получить пользователя
         if ($token) {
             // Находим токен в базе данных
             $tokenModel = \Laravel\Sanctum\PersonalAccessToken::findToken($token);
-            $debugInfo['token_found'] = !empty($tokenModel);
             
             if ($tokenModel) {
                 // Получаем пользователя по токену
                 $user = $tokenModel->tokenable;
-                $debugInfo['user_found'] = !empty($user);
-                $debugInfo['user_type'] = $user ? get_class($user) : null;
                 
                 // Если пользователь - клиент, проверяем его группу
                 if ($user && $user instanceof \Webkul\Customer\Models\Customer) {
                     $customerGroup = app(\Webkul\Customer\Repositories\CustomerGroupRepository::class)->find($user->customer_group_id);
                     $isWholesaleCustomer = $customerGroup && $customerGroup->code === 'wholesale';
-                    
-                    $debugInfo['customer_id'] = $user->id;
-                    $debugInfo['customer_email'] = $user->email;
-                    $debugInfo['customer_group_id'] = $user->customer_group_id;
-                    $debugInfo['customer_group'] = $customerGroup ? [
-                        'id' => $customerGroup->id,
-                        'name' => $customerGroup->name,
-                        'code' => $customerGroup->code,
-                    ] : null;
-                    $debugInfo['is_wholesale'] = $isWholesaleCustomer;
                 }
             }
         }
         
         // Также проверяем сессионную авторизацию
         $sessionAuth = auth()->guard('customer')->check();
-        $debugInfo['session_auth'] = $sessionAuth;
         
         if ($sessionAuth && !$isWholesaleCustomer) {
             $customer = auth()->guard('customer')->user();
             $customerGroup = app(\Webkul\Customer\Repositories\CustomerGroupRepository::class)->find($customer->customer_group_id);
             $isWholesaleCustomer = $customerGroup && $customerGroup->code === 'wholesale';
-            
-            $debugInfo['session_customer_id'] = $customer->id;
-            $debugInfo['session_customer_email'] = $customer->email;
-            $debugInfo['session_customer_group_id'] = $customer->customer_group_id;
-            $debugInfo['session_customer_group'] = $customerGroup ? [
-                'id' => $customerGroup->id,
-                'name' => $customerGroup->name,
-                'code' => $customerGroup->code,
-            ] : null;
-            $debugInfo['session_is_wholesale'] = $isWholesaleCustomer;
         }
 
         /* generating resource */
@@ -123,19 +97,7 @@ class ProductResource extends JsonResource
         /* Добавляем цены для оптовых клиентов, если пользователь из оптовой группы */
         if ($isWholesaleCustomer) {
             $customerGroupPrices = $this->getCustomerGroupPrices($product);
-            $resource['customer_group_prices'] = $customerGroupPrices['prices'];
-        }
-        
-        /* Добавляем отладочную информацию в режиме отладки */
-        if (config('app.debug')) {
-            $resource['auth_debug'] = $debugInfo;
-            
-            // В режиме отладки всегда показываем цены для групп клиентов
-            if (!$isWholesaleCustomer) {
-                $customerGroupPrices = $this->getCustomerGroupPrices($product);
-                $resource['debug_customer_group_prices'] = $customerGroupPrices['prices'];
-                $resource['debug_prices_info'] = $customerGroupPrices['debug_info'];
-            }
+            $resource['customer_group_prices'] = $customerGroupPrices;
         }
 
         /* product's extra information */
@@ -318,41 +280,12 @@ class ProductResource extends JsonResource
     private function getCustomerGroupPrices($product)
     {
         $prices = [];
-        $debug = [];
 
         try {
-            // Добавляем информацию о продукте
-            $debug['product_id'] = $product->id;
-            $debug['product_type'] = $product->type;
-            
-            // Добавляем информацию об авторизации
-            $debug['auth_check'] = auth()->guard('customer')->check();
-            $debug['user_info'] = auth()->guard('customer')->check() ? [
-                'id' => auth()->guard('customer')->user()->id,
-                'email' => auth()->guard('customer')->user()->email,
-                'customer_group_id' => auth()->guard('customer')->user()->customer_group_id,
-            ] : null;
-            
-            // Добавляем информацию о группе клиентов
-            if (auth()->guard('customer')->check()) {
-                $customer = auth()->guard('customer')->user();
-                $customerGroup = app(\Webkul\Customer\Repositories\CustomerGroupRepository::class)->find($customer->customer_group_id);
-                $debug['customer_group'] = $customerGroup ? [
-                    'id' => $customerGroup->id,
-                    'name' => $customerGroup->name,
-                    'code' => $customerGroup->code,
-                ] : null;
-            }
-            
-            // Проверяем наличие репозитория цен для групп клиентов
-            $debug['repository_exists'] = class_exists(\Webkul\Product\Repositories\ProductCustomerGroupPriceRepository::class);
-            
-            // Если репозиторий существует, пытаемся получить цены
+            // Получаем репозиторий цен для групп клиентов
             if (class_exists(\Webkul\Product\Repositories\ProductCustomerGroupPriceRepository::class)) {
                 $customerGroupPriceRepository = app(\Webkul\Product\Repositories\ProductCustomerGroupPriceRepository::class);
                 $customerGroupPrices = $customerGroupPriceRepository->findWhere(['product_id' => $product->id]);
-                
-                $debug['customer_group_prices_count'] = $customerGroupPrices->count();
                 
                 if ($customerGroupPrices->count() > 0) {
                     foreach ($customerGroupPrices as $price) {
@@ -390,46 +323,35 @@ class ProductResource extends JsonResource
                         ];
                     }
                 }
-            } else {
-                $debug['error'] = 'ProductCustomerGroupPriceRepository не существует';
-                
+            } else if (property_exists($product, 'customer_group_prices') && $product->customer_group_prices) {
                 // Пробуем получить цены через свойство продукта
-                if (property_exists($product, 'customer_group_prices') && $product->customer_group_prices) {
-                    $debug['has_customer_group_prices_property'] = true;
-                    $debug['customer_group_prices_count'] = count($product->customer_group_prices);
+                foreach ($product->customer_group_prices as $price) {
+                    $customerGroup = app(\Webkul\Customer\Repositories\CustomerGroupRepository::class)->find($price->customer_group_id);
                     
-                    foreach ($product->customer_group_prices as $price) {
-                        $customerGroup = app(\Webkul\Customer\Repositories\CustomerGroupRepository::class)->find($price->customer_group_id);
-                        
-                        if (!$customerGroup) {
-                            continue;
-                        }
-                        
-                        $prices[] = [
-                            'customer_group_id' => $price->customer_group_id,
-                            'customer_group_code' => $customerGroup->code,
-                            'customer_group_name' => $customerGroup->name,
-                            'qty' => $price->qty,
-                            'value_type' => $price->value_type,
-                            'value' => $price->value,
-                            'calculated_price' => $this->calculatePrice($product, $price),
-                            'formatted_calculated_price' => core()->currency($this->calculatePrice($product, $price)),
-                        ];
+                    if (!$customerGroup) {
+                        continue;
                     }
-                } else {
-                    $debug['has_customer_group_prices_property'] = false;
+                    
+                    $prices[] = [
+                        'customer_group_id' => $price->customer_group_id,
+                        'customer_group_code' => $customerGroup->code,
+                        'customer_group_name' => $customerGroup->name,
+                        'qty' => $price->qty,
+                        'value_type' => $price->value_type,
+                        'value' => $price->value,
+                        'calculated_price' => $this->calculatePrice($product, $price),
+                        'formatted_calculated_price' => core()->currency($this->calculatePrice($product, $price)),
+                    ];
                 }
             }
         } catch (\Exception $e) {
-            $debug['error'] = $e->getMessage();
-            $debug['trace'] = $e->getTraceAsString();
+            // Логирование ошибки, если необходимо
+            if (config('app.debug')) {
+                \Log::error('Ошибка при получении цен для групп клиентов: ' . $e->getMessage());
+            }
         }
         
-        // Всегда возвращаем отладочную информацию
-        return [
-            'prices' => $prices,
-            'debug_info' => $debug
-        ];
+        return $prices;
     }
 
     /**
